4ad692b8bb8455651b78e5e8287345e6814650a9
daniel-albuquerque <albuquerquedaniel898@gmail.com>
2024-12-31 10:25:56 -0300

    test: view home page

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from .models import Company, Employee, TimeRecord
 from datetime import datetime, time
 
@@ -58,3 +59,78 @@ class TimeRecordModelTest(TestCase):
         self.time_record.check_out = time(19, 0)
         self.time_record.save()
         self.assertEqual(self.time_record.calculate_overtime(), 1.0)
+
+
+class HomeViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.company = Company.objects.create(name="Empresa Teste", address="Endereço Teste", phone="123456789")
+        self.employee = Employee.objects.create(
+            name="Funcionário Teste",
+            email="teste@empresa.com",
+            company=self.company,
+            standard_check_in=time(9, 0),
+            standard_check_out=time(18, 0),
+            auth_code="123456"
+        )
+
+    def test_home_get(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_post_check_in(self):
+        response = self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'check_in'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Ação check_in concluída para Funcionário Teste")
+
+    def test_home_post_invalid_auth_code(self):
+        response = self.client.post(reverse('home'), {
+            'auth_code': 'invalid_code',
+            'action': 'check_in'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(response.context['message'], "Ocorreu um erro: No Employee matches the given query.")
+
+    def test_home_post_start_break(self):
+        self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'check_in'
+        })
+        response = self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'start_break'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Ação start_break concluída para Funcionário Teste")
+
+    def test_home_post_end_break(self):
+        self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'check_in'
+        })
+        self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'start_break'
+        })
+        response = self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'end_break'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Ação end_break concluída para Funcionário Teste")
+
+    def test_home_post_check_out(self):
+        self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'check_in'
+        })
+        response = self.client.post(reverse('home'), {
+            'auth_code': self.employee.auth_code,
+            'action': 'check_out'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Ação check_out concluída para Funcionário Teste")
